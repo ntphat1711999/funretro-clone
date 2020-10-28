0a24776f1257d50d46edcd8a06d54276a0eff614
Phat Nguyen <tanphat1711999@gmail.com>
2020-10-28 20:24:09 +0700

    fix path static file

--- app/database/db.sql
+++ app/database/db.sql
@@ -23,3 +23,10 @@ CREATE TABLE card (
       REFERENCES board(id) 
       ON DELETE CASCADE
 );
+
+INSERT INTO board (name, date, owner)
+VALUES 
+  ('HEL-1', '2020-10-21', 'demo@gmail.com'),
+  ('HEL-2', '2020-10-22', 'demo@gmail.com'),
+  ('HEL-3', '2020-10-28', 'demo1@gmail.com'),
+  ('HEL-4', '2020-10-24', 'demo1@gmail.com')

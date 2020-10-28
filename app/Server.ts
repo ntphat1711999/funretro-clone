24426a4b671cca399477aa4e654a42e3144d3d36
Phat Nguyen <tanphat1711999@gmail.com>
2020-10-28 20:31:04 +0700

    fix path static file

--- app/Server.ts
+++ app/Server.ts
@@ -28,7 +28,7 @@ export class Server {
     this.app.use("/api/boards", boardRoute);
 
     this.app.get("*", (req: Request, res: Response): void => {
-      res.sendFile(path.join(__dirname + "../build/frontend/index.html"));
+      res.sendFile("/app/build/frontend/index.html");
     });
   }
 
